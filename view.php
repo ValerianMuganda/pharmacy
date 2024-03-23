7bdb5890844c6128bd36dd5bafc34a85c7b230a4
Valerian Muganda <valerianmuganda97@gmail.com>
2024-03-23 15:05:12 +0300

    created a file to view the drugs

--- view.php
+++ view.php
@@ -8,6 +8,26 @@
 <body>
     <h1>This is the page to show drug details</h1>
     <p>Testing</p>
+    <?php
+    $id = intval($_GET["id"]);
+
+    $connection = new mysqli("localhost","root","","pharmacy",3307);
+    $query = $connection->query("SELECT * FROM drugs WHERE id =$id");
     
+
+    if($query->num_rows >0){
+        while($drug=$query->fetch_assoc()){
+            echo "<h2>Drug Details</h2>";
+            $name = $drug['name'];
+            $price = $drug['price'];
+            $description = $drug['description'];  
+
+            echo "Name: " . "<b>$name</b> <br>";
+            echo "price: " . "<b>TZS $price</b> <br>";
+            echo "Description: " . "<b> $description</b> <br>";
+        }
+    }
+
+    ?>
 </body>
 </html>

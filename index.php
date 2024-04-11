937dc7a9132698460e1d33bce480a6b4e70c7273
Louis Richard <luisrichard2580@gmail.com>
2024-04-11 21:25:34 +0300

    Styled The Table

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pharmacy Management System</title>
+    <style>
+        td,
+        th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        tr:hover {
+            background-color: #ddd;
+        }
+
+        th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #4CAF50;
+            color: white;
+        }
+    </style>
 </head>
+
 <body>
     <h1>Welcome To Valerian Pharmacy</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, ipsa doloremque pariatur quasi illum ullam, debitis alias officia temporibus animi deleniti molestiae nulla unde maiores veniam exercitationem eum mollitia quo.</p>
@@ -13,21 +38,24 @@
     <table border="1px">
         <thead>
             <tr>
-                <td>Id</td><td>Name</td><td>Price</td><td>Action</td>
+                <td>Id</td>
+                <td>Name</td>
+                <td>Price</td>
+                <td>Action</td>
             </tr>
         </thead>
-        
+
         <tbody>
             <?php
-                include("connection.php");
-                $drugs = $connection->query("SELECT * FROM drugs");
+            include("connection.php");
+            $drugs = $connection->query("SELECT * FROM drugs");
 
-                if($drugs->num_rows>0){
-                    while($drug = $drugs->fetch_assoc()){
-                        $id = $drug["id"];
-                        $name = $drug["name"];
-                        $price = $drug["price"];
-                        echo <<<ROW
+            if ($drugs->num_rows > 0) {
+                while ($drug = $drugs->fetch_assoc()) {
+                    $id = $drug["id"];
+                    $name = $drug["name"];
+                    $price = $drug["price"];
+                    echo <<<ROW
                             <tr>
                                 <td>$id</td>
                                 <td>$name</td>
@@ -41,8 +69,8 @@
                             </tr>
 
                         ROW;
-                    }
                 }
+            }
             ?>
             <script src="pharmacy.js></script>
         </tbody>
